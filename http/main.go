91bad061502888433dceb6343814296cc7c88748
daison32 <sunagimogushi0406@gmail.com>
2022-10-11 23:19:35 +0900

    update main.go

--- http/main.go
+++ http/main.go
@@ -29,26 +29,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write(bytes)
 }
 
-func handler2(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	bytes, err := json.Marshal(responseMessage{
-		Message: "Hello, world!",
-	})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Write(bytes)
-}
-
 func main() {
 	http.HandleFunc("/hello", handler)
-	http.HandleFunc("/", handler2)
 	http.ListenAndServe(":8000", nil)
 }
