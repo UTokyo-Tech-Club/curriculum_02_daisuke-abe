02efb3fe2960af6261989efa211db008cd39770b
daison32 <106550517+daison32@users.noreply.github.com>
2022-10-18 11:33:31 +0900

    Don't know why I can't see response.data

--- [REACT]react-to-go/my-app-ts/src/App.tsx
+++ [REACT]react-to-go/my-app-ts/src/App.tsx
@@ -1,28 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Form from "./Form";
+import axios from "axios";
 
 function App() {
-  const onSubmit = (name: string, email: string) => {
-    console.log("onSubmit:", name, " ", email);
+  const onSubmit = (name: string, age: number) => {
+    console.log("onSubmit:", name, " ", age);
+    axios.post("http://localhost:8000/user", {
+      name: name,
+      age: age,
+    });
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Form onSubmit={onSubmit}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="title">User Register</div>
+        <Form onSubmit={onSubmit} />
       </header>
     </div>
   );
@@ -33,5 +27,5 @@ export default App;
 // 「何故submit.preventDefault()という処理を入れる必要があるのか」
 // 　　　->submitに対するブラウザのデフォルトの反応が効いてしまい、submit毎に入力欄が空になってしまうため
 
-// 「inputに対してvalueで値を指定するとどういうメリットがあるのか」　
+// 「inputに対してvalueで値を指定するとどういうメリットがあるのか」
 // 　 ->入力された値を取り出して、useStateに投げることができる
--- [REACT]react-to-go/my-app-ts/src/Form.tsx
+++ [REACT]react-to-go/my-app-ts/src/Form.tsx
@@ -1,34 +1,80 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+import user from "./user.json"; //レスポンスのJSON(詳しくは補足で)
+const url = "http://localhost:8000";
+
+type USER = typeof user; //画面に表示するユーザー情報の型
 
 type Props = {
-  onSubmit: (name: string, email: string) => void;
-}
+  onSubmit: (name: string, age: number) => void;
+};
 
 const Form = (props: Props) => {
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    // axios.get<USER>(`${url}/users`)
+    //   .then((res) => {
+    //     const { name, age } = res;
+    //     setName(name);
+    //     setAge(age);
+    //   })
+    //   .catch((e: AxiosError<{ error: string }>) => {
+    //     // エラー処理
+    //     console.log(e.message);
+    //   });
+  //   axios.get("http://localhost:8000/users")
+      
+  //   console.log("aaa")
+  // 
+  
+  axios.get('http://localhost:8000/users')
+    .then(response => {
+      console.log('status:', response.status);
+      console.log('body:', response.data)
+    })
+  
+  // Starting Request: {リクエスト内容} <- consoleに出力される
+  
+  // Response: {レスポンス内容} <- consoleに出力される
+
+}, []);
 
   const submit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    props.onSubmit(name, email);
+    props.onSubmit(name, age);
   };
 
   return (
-    <form style={{ display: "flex", flexDirection: "column" }}>
-      <label>Name: </label>
-      <input
-        type={"text"}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      ></input>
-      <label>Email: </label>
-      <input
-        type={"email"}
-        style={{ marginBottom: 20 }}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      ></input>
-      <button onClick={submit}>Submit</button>
+    <form className="form">
+      <div className="row">
+        <label className="label">Name: </label>
+        <input
+          className="inputBox"
+          type={"text"}
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+      </div>
+      <div className="row">
+        <label className="label">Age: </label>
+        <input
+          className="inputBox"
+          type={"number"}
+          id="age"
+          name="age"
+          value={age}
+          onChange={(e) => setAge(Number(e.target.value))}
+        ></input>
+      </div>
+      <button className="button" onClick={submit}>
+        POST
+      </button>
+      <div></div>
     </form>
   );
 };
