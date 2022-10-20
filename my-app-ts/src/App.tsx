2e65373f806ef602f0676260d8468bb9f3268c11
daison32 <106550517+daison32@users.noreply.github.com>
2022-10-20 22:33:26 +0900

    import Form

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Form from "./Form.js";
+import Form from "./Form";
 
 function App() {
   const onSubmit = (name: string, email: string) => {
