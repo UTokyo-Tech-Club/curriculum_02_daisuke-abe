40e7ea5c72663e64cd5a69d6dd8bde10127edce3
daison32 <106550517+daison32@users.noreply.github.com>
2022-10-23 18:18:32 +0900

    comment added on fizzbuzz

--- ut/fizzbuzz.go
+++ ut/fizzbuzz.go
@@ -40,3 +40,7 @@ func main() {
 	ret := FizzBuzz([]int{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
 	fmt.Printf("%v", ret)
 }
+
+
+// test ci
+// added test
