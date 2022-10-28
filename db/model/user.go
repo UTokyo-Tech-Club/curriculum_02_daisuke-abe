20654f20cf3d2c43e98a03cabe7bd2fd64154152
daison32 <106550517+daison32@users.noreply.github.com>
2022-10-28 18:14:38 +0900

    directory and files created

---
+++ db/model/user.go
@@ -0,0 +1,11 @@
+package dao
+type UserResForHTTPGet struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type UserResForHTTPPost struct {
+	Name string
+	Age  int
+}
