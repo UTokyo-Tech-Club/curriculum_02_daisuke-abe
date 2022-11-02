b7d17d9c2cd7e049f8f59b0a4ca196c1a02a943b
daison32 <106550517+daison32@users.noreply.github.com>
2022-11-02 21:55:57 +0900

    CORS added

--- hackathon/main.go
+++ hackathon/main.go
@@ -11,6 +11,7 @@ import (
 	"syscall"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"github.com/oklog/ulid/v2"
 )
 
@@ -30,23 +31,20 @@ var db *sql.DB
 
 func init() {
 	// ①-1
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PWD")
-	mysqlHost := os.Getenv("MYSQL_HOST")
-	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	mysqlUser := os.Getenv("mysqlUser")
+	mysqlUserPwd := os.Getenv("mysqlUserPwd")
+	mysqlDatabase := os.Getenv("mysqlDatabase")
 
-	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	_db, err := sql.Open("mysql", connStr)
+	// ①-2
+	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlUserPwd, mysqlDatabase))
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
 	}
-	// // ①-2
-
 	// ①-3
 	if err := _db.Ping(); err != nil {
 		log.Fatalf("fail: _db.Ping, %v\n", err)
@@ -54,9 +52,58 @@ func init() {
 	db = _db
 }
 
+func handler1(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	switch r.Method {
+	case http.MethodOptions:
+		w.WriteHeader(http.StatusOK)
+		return
+	case http.MethodGet:
+		rows, err := db.Query("SELECT * FROM user")
+		if err != nil {
+			log.Printf("fail: db.Query, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		users := make([]UserResForHTTPGet, 0)
+		for rows.Next() {
+			var u UserResForHTTPGet
+			if err := rows.Scan(&u.Id, &u.Name, &u.Age); err != nil {
+				log.Printf("fail: rows.Scan, %v\n", err)
+
+				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
+					log.Printf("fail: rows.Close(), %v\n", err)
+				}
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			users = append(users, u)
+		}
+
+		// ②-4
+		bytes, err := json.Marshal(users)
+		if err != nil {
+			log.Printf("fail: json.Marshal, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(bytes)
+	}
+}
+
 // ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
 func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
 	switch r.Method {
+	case http.MethodOptions:
+		w.WriteHeader(http.StatusOK)
+		return
 	case http.MethodGet:
 		// ②-1
 		name := r.URL.Query().Get("name") // To be filled
@@ -103,11 +150,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPost:
 		var u UserResForHTTPPost
 		fmt.Println("got POST method")
+
 		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			fmt.Println("Decode失敗")
+			fmt.Printf("%+v\n", u)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
 		if u.Name == "" || len(u.Name) > 50 || u.Age < 20 || u.Age > 80 {
 			fmt.Println("Name and age don't meet the format")
 			w.WriteHeader(http.StatusBadRequest)
@@ -149,14 +198,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
+	// POSTもこっち
 	http.HandleFunc("/user", handler)
+
+	// ラスト課題：/users にGETリクエストを送ると、USERテーブルの全レコードを返す
+	http.HandleFunc("/users", handler1)
 
 	// ③ Ctrl+CでHTTPサーバー停止時にDBをクローズする
 	closeDBWithSysCall()
 
 	// 8000番ポートでリクエストを待ち受ける
 	log.Println("Listening...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
