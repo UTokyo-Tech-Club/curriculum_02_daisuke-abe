aadb709d7a4ef04ed89be8be09c31ba88a6d8795
daison32 <106550517+daison32@users.noreply.github.com>
2022-11-19 00:01:39 +0900

    transactions divided

--- hackathon/main.go
+++ hackathon/main.go
@@ -12,7 +12,6 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	// "github.com/joho/godotenv"
-	"github.com/oklog/ulid/v2"
 )
 
 type UserResForHTTPGet struct {
@@ -84,215 +83,6 @@ func init() {
 	db = _db
 }
 
-func list(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	switch r.Method {
-	case http.MethodOptions:
-		w.WriteHeader(http.StatusOK)
-		return
-	case http.MethodGet:
-		rows, err := db.Query("SELECT * FROM transaction")
-		if err != nil {
-			log.Printf("fail: db.Query, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		users := make([]TransactionGet, 0)
-		for rows.Next() {
-			var u TransactionGet
-			if err := rows.Scan(&u.Id, &u.Fromwhom, &u.Towhom, &u.Message, &u.Point); err != nil {
-				log.Printf("fail: rows.Scan, %v\n", err)
-
-				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-					log.Printf("fail: rows.Close(), %v\n", err)
-				}
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			users = append(users, u)
-		}
-
-		// ②-4
-		bytes, err := json.Marshal(users)
-		if err != nil {
-			log.Printf("fail: json.Marshal, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(bytes)
-
-	case http.MethodPost:
-		var u TransactionPost
-		fmt.Println("got POST method")
-
-		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-			fmt.Println("Decode失敗")
-			fmt.Printf("%+v\n", u)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Printf("%+v\n", u)
-
-		ins, err := db.Prepare("INSERT INTO transaction VALUES(?, ?, ?, ?, ?)")
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		defer ins.Close()
-		fmt.Println("SQL prepared")
-
-		id := ulid.Make()
-		res, err := ins.Exec(id.String(), u.Fromwhom, u.Towhom, u.Message, u.Point)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		fmt.Println("inserted to DB")
-
-		lastInsertID, err := res.LastInsertId()
-		if err != nil {
-			log.Fatal(err)
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-		w.WriteHeader(http.StatusOK)
-		log.Println(lastInsertID)
-		fmt.Println("id: " + id.String())
-
-	case http.MethodDelete:
-		var u TransactionDelete
-		fmt.Println("got Delete method")
-
-		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-			fmt.Println("Decode失敗")
-			fmt.Printf("%+v\n", u)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		ins, err := db.Prepare("DELETE FROM transaction WHERE id = (?);")
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		defer ins.Close()
-		fmt.Println("SQL prepared")
-
-		if _, err := ins.Exec(u.Id); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Println(u.Id + "Deleted from DB")
-
-		if err != nil {
-			log.Fatal(err)
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-		w.WriteHeader(http.StatusOK)
-	}
-}
-
-// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Access-Control-Allow-Headers", "*")
-// 	w.Header().Set("Access-Control-Allow-Origin", "*")
-// 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-
-// 	switch r.Method {
-// 	case http.MethodOptions:
-// 		w.WriteHeader(http.StatusOK)
-// 		return
-// 	case http.MethodGet:
-// 		// ②-1
-// 		name := r.URL.Query().Get("name") // To be filled
-// 		if name == "" {
-// 			log.Println("fail: name is empty")
-// 			w.WriteHeader(http.StatusBadRequest)
-// 			return
-// 		}
-
-// 		// ②-2
-// 		rows, err := db.Query("SELECT id, name, age FROM user WHERE name = ?", name)
-// 		if err != nil {
-// 			log.Printf("fail: db.Query, %v\n", err)
-// 			w.WriteHeader(http.StatusInternalServerError)
-// 			return
-// 		}
-
-// 		// ②-3
-// 		users := make([]UserResForHTTPGet, 0)
-// 		for rows.Next() {
-// 			var u UserResForHTTPGet
-// 			if err := rows.Scan(&u.Id, &u.Name, &u.Age); err != nil {
-// 				log.Printf("fail: rows.Scan, %v\n", err)
-
-// 				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-// 					log.Printf("fail: rows.Close(), %v\n", err)
-// 				}
-// 				w.WriteHeader(http.StatusInternalServerError)
-// 				return
-// 			}
-// 			users = append(users, u)
-// 		}
-
-// 		// ②-4
-// 		bytes, err := json.Marshal(users)
-// 		if err != nil {
-// 			log.Printf("fail: json.Marshal, %v\n", err)
-// 			w.WriteHeader(http.StatusInternalServerError)
-// 			return
-// 		}
-// 		w.Header().Set("Content-Type", "application/json")
-// 		w.Write(bytes)
-
-// 	// case http.MethodPost:
-// 	// 	var u TransactionPost
-// 	// 	fmt.Println("got POST method")
-
-// 	// 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-// 	// 		fmt.Println("Decode失敗")
-// 	// 		fmt.Printf("%+v\n", u)
-// 	// 		w.WriteHeader(http.StatusInternalServerError)
-// 	// 		return
-// 	// 	}
-
-// 	// 	fmt.Printf("%+v\n", u)
-
-// 	// 	ins, err := db.Prepare("INSERT INTO transaction VALUES(?, ?, ?, ?, ?)")
-// 	// 	if err != nil {
-// 	// 		w.WriteHeader(http.StatusInternalServerError)
-// 	// 		return
-// 	// 	}
-// 	// 	defer ins.Close()
-// 	// 	fmt.Println("SQL prepared")
-
-// 	// 	id := ulid.Make()
-// 	// 	res, err := ins.Exec(id.String(), u.Fromwhom, u.Towhom, u.Message, u.Point)
-// 	// 	if err != nil {
-// 	// 		w.WriteHeader(http.StatusInternalServerError)
-// 	// 		return
-// 	// 	}
-// 	// 	fmt.Println("inserted to DB")
-
-// 	// 	lastInsertID, err := res.LastInsertId()
-// 	// 	if err != nil {
-// 	// 		log.Fatal(err)
-// 	// 		w.WriteHeader(http.StatusInternalServerError)
-// 	// 	}
-// 	// 	w.WriteHeader(http.StatusOK)
-// 	// 	log.Println(lastInsertID)
-// 	// 	fmt.Println("id: " + id.String())
-// 	default:
-// 		log.Printf("fail: HTTP Method is %s\n", r.Method)
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		return
-// 	}
-// }
-
 func edit(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -333,7 +123,12 @@ func edit(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 		w.WriteHeader(http.StatusOK)
+	default:
+		log.Printf("fail: HTTP Method is %s\n", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+
 }
 
 func points(w http.ResponseWriter, r *http.Request) {
@@ -384,7 +179,7 @@ func main() {
 	// http.HandleFunc("/transaction", handler)
 
 	// /transactions で取引の全履歴をJsonで返す
-	http.HandleFunc("/transactions", list)
+	http.HandleFunc("/transactions", Transactions)
 
 	// 貢献の編集
 	http.HandleFunc("/edit", edit)
