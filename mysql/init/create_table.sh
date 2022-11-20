07b77d16d8a28e1fdffa7a9d368a578be3121d92
daison32 <sunagimogushi0406@gmail.com>
2022-11-20 17:43:55 +0900

    wip

--- mysql/init/create_table.sh
+++ mysql/init/create_table.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 CMD_MYSQL="mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
-$CMD_MYSQL -e "create table article (
+$CMD_MYSQL -e "create table transaction (
     id int(10)  AUTO_INCREMENT NOT NULL primary key,
-    title varchar(50) NOT NULL,
-    body varchar(1000)
+    fromwhom varchar(50),
+    towhom varchar(50),
+    message varchar(200),
+    point int(10)
     );"
-$CMD_MYSQL -e  "insert into article values (1, '記事1', '記事1です。');"
-$CMD_MYSQL -e  "insert into article values (2, '記事2', '記事2です。');"
+
+$CMD_MYSQL -e "create table user (
+id int(10)  AUTO_INCREMENT NOT NULL primary key,
+name varchar(50),
+);"
+
+# $CMD_MYSQL -e  "insert into transaction values (, '記事1', '記事1です。');"
+# $CMD_MYSQL -e  "insert into article values (2, '記事2', '記事2です。');"
